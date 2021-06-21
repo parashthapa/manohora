d99f6d552c8c7c54a3834d9c1e6242b64627f157
paras <parashmagarme@gmail.com>
2021-06-21 15:18:07 +0545

    Backup for manohara

---
+++ js/custom.js
@@ -0,0 +1,40 @@
+    (function($){
+    $('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
+      if (!$(this).next().hasClass('show')) {
+        $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+      }
+      var $subMenu = $(this).next(".dropdown-menu");
+      $subMenu.toggleClass('show');
+
+      $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+        $('.dropdown-submenu .show').removeClass("show");
+      });
+
+      return false;
+    });
+})(jQuery)
+
+
+
+
+$(window).on('scroll', function () {
+        if ($(this).scrollTop() > 600) {
+            $('.scroll-top').removeClass('not-visible');
+        } else {
+            $('.scroll-top').addClass('not-visible');
+        }
+    });
+    $('.scroll-top').on('click', function (event) {
+        $('html,body').animate({
+            scrollTop: 0
+        }, 1000);
+    });
+    
+
+$(document).ready(function() {
+
+$('#myCarouselArticle').carousel({
+                interval: 5000
+        });
+  
+});
